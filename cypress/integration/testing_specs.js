717d7109932f2fa1c977212ff3b536583403e96a
jdhillon95 <jdhillon95@gmail.com>
2019-03-26 12:14:08 -0700

    Changes

--- cypress/integration/testing_specs.js
+++ cypress/integration/testing_specs.js
@@ -1,22 +1,50 @@
 
-describe('SacStateWebsite', function(){
-	it('Visits Sac State Website', function(){
+describe('Top Nav Bar', function(){
+	it('Top Bar Sac State Website', function(){
 		cy.visit('https://csus.edu')
 
-		//Header
-		
 		cy.contains('Directories').click()
 		cy.contains('Calendars').click()
 		cy.contains('Quick Links').click()
+				
+})
+})
+describe('Login - My Sac state', function() {
+	it('Header  Bar at Top Sac state', function() {
+		cy.visit('https://my.csus.edu')
+		cy.get('input[name=username]').type('TYPE IN USERNAME').blur()
+		cy.get('input[name=password]').type('TYPE IN PASSWORD').blur()
+		cy.contains('Login').click()
+		})
+	})
+
+describe('Nav Bar 1', function(){
+	it('Milldle Nav Bar Sac State Website', function(){
+		cy.visit('https://csus.edu')
 
 		//Click navigation menu on Top
+		cy.contains('Admissions').get('.dropdown')
 		cy.contains('About').click()
 		cy.contains('Student Life').click()
-		cy.contains('Make a Gift').click()
-		cy.get('.dropdown-menu').click()
+		
+		
+	})
+})
+/*
+describe('Nav Bar 2', function(){
+	it('Milldle of page', function(){
+		cy.visit('https://csus.edu')
 
-		//Middle Navigation Bar
 		
 		
+	})
 })
+describe('Footer', function(){
+	it('Bottom of the Page', function(){
+		cy.visit('https://csus.edu')
+
+		cy.contains('Housing')
+		
+	})
 })
+*/
